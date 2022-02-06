9ce0edf9f4c4e90d25b68ab5c49dc0ee5ac8f69a
Tartios <tartios@yandex.ru>
2022-02-06 12:01:31 +0300

    recreate app

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from './Input';
+import Button from './Button';
+
+
+export default function Form() {
+    return (
+        <form action="">
+            <Input />
+            <Button value="Добавить" />
+        </form>
+    )
+}
